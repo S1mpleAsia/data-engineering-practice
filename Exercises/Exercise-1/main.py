ca74cec59f6eccee29d35cab470f07ade512a338
S1mpleAsia <duongk65bkhn@gmail.com>
2024-11-01 23:51:52 +0700

    update final  Exercise-1

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,6 +1,10 @@
 import requests, zipfile
 import os
-import io
+import aiohttp
+import asyncio
+import aiofiles
+from concurrent.futures import ThreadPoolExecutor
+import time
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -26,6 +30,7 @@ def create_storage_folder():
                 os.rmdir(os.path.join(root, name))
 
 def download_file_http():
+    start = time.perf_counter()
     for i, uri in enumerate(download_uris):
             try:
                 print(f'---- Start download file: {i+1} with uri: {uri} ----')
@@ -36,7 +41,7 @@ def download_file_http():
                 
             except Exception as e:
                 print(f'Exception when download file {i+1} - error: {e}')
-    print("\n")
+    print(f'\nTotal time download with thread pool: {time.perf_counter() - start}\n')
     
 
 def extract_csv_file():
@@ -63,11 +68,72 @@ def main():
     download_file_http()
 
     # 3. Extract csv from zip file
-    extract_csv_file()
+    # extract_csv_file()
 
-def async_http():
-    pass
+
+# ---- Thread pool executor with requests download ----
+
+def download_single_file_http(uri):
+    try:
+        print(f'---- Start download file with uri: {uri} ----')
+        r = requests.get(uri)
+        with open(directory + "/" + uri.split("/")[-1], "wb") as fd:
+            for chunk in r.iter_content(chunk_size=128):
+                fd.write(chunk)            
+    except Exception as e:
+        print(f'Exception when download file - error: {e}')
+
+def thread_pool_download():
+    start = time.perf_counter()
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(download_single_file_http, download_uris)
+
+    print(f'\nTotal time download with thread pool: {time.perf_counter() - start}\n')
+
+
+def thread_pool_main():
+    # 1. Create directory downloads if not exists
+    create_storage_folder()
+
+    # 2. Download file from uri sequentially
+    thread_pool_download()
+
+    # 3. Extract csv from zip file
+    # extract_csv_file()
+
+# ----- End thread pool executor download -----
+
+
+# ----- Async IO download method -----
+def async_main():
+    create_storage_folder()
+
+    start = time.perf_counter()
+    sema = asyncio.Semaphore(5)
+
+    async def fetch_file(session: aiohttp.ClientSession, uri):
+        file_name = directory + "/" + uri.split("/")[-1]
+        print(f'---- Start download file with uri: {uri} ----')
+        async with sema:
+            async with session.get(uri) as resp:
+                data = await resp.read()
+        
+        async with aiofiles.open(file_name, "wb") as output_file:
+            await output_file.write(data)
+    
+    async def main():
+        async with aiohttp.ClientSession() as session:
+            tasks = [fetch_file(session, uri) for uri in download_uris]
+            await asyncio.gather(*tasks)
+    
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
+    print(f'\nTotal time download with asyncio: {time.perf_counter() - start}\n')
+# ----- End async IO download method -----
 
 
 if __name__ == "__main__":
     main()
+    thread_pool_main()
+    async_main()
---
+++ Exercises/Exercise-1/result.md
@@ -0,0 +1,11 @@
+### With requests
+
+Total time download is `103.12` seconds
+
+### With thread pool
+
+Total time download is `63.47` seconds
+
+### With asyncio
+
+Total time download is `64.17` seconds
